1b53653b85653f5402065d7028549400391c3e19
Xiangming Wang <washiwxm@gmail.com>
2016-10-15 11:19:29 +0800

    高中等差数组求和

---
+++ missing-number.cc
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/missing-number/
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+  		int total_should = (0 + nums.size()) * (nums.size() + 1) / 2;
+		int total_now = 0;
+		for (int i = 0; i < nums.size(); ++i) {
+			total_now += nums[i];
+		}
+		return total_should - total_now;
+    }
+};
