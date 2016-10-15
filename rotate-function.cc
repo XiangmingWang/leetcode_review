12b3dcab7ce26c1f4dfabcbbe0c45d364339a73b
Xiangming Wang <washiwxm@gmail.com>
2016-10-15 13:25:34 +0800

    虽然是easy,我提交错了好多次

---
+++ rotate-function.cc
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/rotate-function/
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& A) {
+		if (A.empty()) return 0;
+		int result = INT_MIN, sum = 0, tmp = 0;
+		for (int i = 0; i < A.size(); ++i) {
+			sum += A[i];
+			tmp += A[i] * i;
+		}
+		result = max(result, tmp);
+		for (int i = 0; i < A.size(); ++i){
+			tmp = tmp - sum + A.size() * A[i];
+			result = max(result, tmp);
+		}
+		return result;
+    }
+};
