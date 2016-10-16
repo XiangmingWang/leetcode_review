661b125427eced7872c8aac8a791c04e46d9b1c8
Xiangming Wang <washiwxm@gmail.com>
2016-10-16 16:29:17 +0800

    longest-common-prefix

---
+++ longest-common-prefix.cc
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/longest-common-prefix/
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+		if (strs.empty()) return "";
+   		int i = 0;
+		char tmp;
+		while (true) {
+			if (i == strs[0].size()) return strs[0].substr(0, i);
+			tmp = strs[0][i];
+			for (int j = 1; j < strs.size(); ++j) {
+				if (i >= strs[j].size() || strs[j][i] != tmp) return strs[0].substr(0, i);
+			}
+			++i;
+		}     
+		return strs[0];
+    }
+};
