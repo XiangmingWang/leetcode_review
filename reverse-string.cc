2b74fd49f3046f800a8031eec41da070122d87a5
Xiangming Wang <washiwxm@gmail.com>
2016-10-23 18:48:23 +0800

    不想做了，刷点水题，完成任务

---
+++ reverse-string.cc
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/reverse-string/
+class Solution {
+public:
+    string reverseString(string s) {
+		if (s.empty()) return s;
+        string result = s;
+		for (int i = 0, j = s.size() - 1; i < s.size(); ++i, --j) {
+			result[i] = s[j];
+		}
+		return result;
+    }
+};
