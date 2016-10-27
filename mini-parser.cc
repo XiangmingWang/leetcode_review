a12b6e815d58ea63eb7441d8a4a35ac7affec6dd
Xiangming Wang <washiwxm@gmail.com>
2016-10-27 23:18:25 +0800

    mini-parser

---
+++ mini-parser.cc
@@ -0,0 +1,58 @@
+//https://leetcode.com/problems/mini-parser/
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+public:
+    NestedInteger deserialize(string s) {
+        if (s[0] != '[') return NestedInteger(stoi(s));
+        NestedInteger* cur = NULL;
+        stack<NestedInteger*> stk;
+        size_t left = 0;
+        for (size_t i = 0; i < s.size(); ++i) {
+            char c = s[i];
+            if (c == '[') {
+                stk.push(new NestedInteger());
+                left = i + 1;
+            } else if (c == ']' || c == ',') {
+                if (left != i) {
+                    string tmp = s.substr(left, i - left);
+                    stk.top()->add(NestedInteger(stoi(tmp)));
+                }
+                left = i + 1;
+                if (c == ']' && stk.size() > 1) {
+                    cur = stk.top();
+                    stk.pop();
+                    stk.top()->add(*cur);
+                }
+            }
+        }      
+        return *stk.top();
+    }
+};
