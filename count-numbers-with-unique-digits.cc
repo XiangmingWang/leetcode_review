eef0acade32156b4f73369e1a01424fa5e522141
Xiangming Wang <washiwxm@gmail.com>
2016-11-06 16:56:53 +0800

    count-numbers-with-unique-digits

---
+++ count-numbers-with-unique-digits.cc
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/count-numbers-with-unique-digits/
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) return 1;
+        int res = 0;
+        for (int i = 1; i <= n; ++i) {
+            res += count(i);
+        }
+        return res;
+    }
+    int count(int k) {
+        if (k < 1) return 0;
+        if (k == 1) return 10;
+        int res = 1;
+        for (int i = 9; i >= (11 - k); --i) {
+            res *= i;
+        }
+        return res * 9;
+    }
+};
