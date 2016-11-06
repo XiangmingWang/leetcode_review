667787b377adde2aa0000ed2c032a8c21876b192
Xiangming Wang <washiwxm@gmail.com>
2016-11-06 16:47:50 +0800

    以前有人说计算机应该是3进制，因为3更接近e,所以有3用3，没3用2

---
+++ integer-break.cc
@@ -0,0 +1,7 @@
+//https://leetcode.com/problems/integer-break/
+class Solution {
+public:
+    int integerBreak(int n) {
+        return n < 4 ? n - 1 : n%3 == 0 ? pow(3,n/3) : pow(3,n/3-1) * (2+2*(n%3));      
+    }
+};
