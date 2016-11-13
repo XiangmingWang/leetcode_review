95940ee4a30acf177f9d0a15aae2c6ae6afe8926
Xiangming Wang <washiwxm@gmail.com>
2016-11-13 17:23:14 +0800

    binary search

---
+++ guess-number-higher-or-lower.cc
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/guess-number-higher-or-lower/
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int left = 1, right = n, mid;
+        while (left <= right) {
+            mid = left + (right - left)/2;
+            switch(guess(mid)) {
+            case 0:
+                return mid;
+            case 1:
+                left = mid + 1;
+                break;
+            case -1:
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
